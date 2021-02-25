383502c220ef2de62a136d5b2415d6598459b767
PhilDanW <42147737+PhilDanW@users.noreply.github.com>
2021-02-25 12:41:47 -0600

    Create shared.h

---
+++ shared.h
@@ -0,0 +1,25 @@
+#ifndef SHARED_H
+#define SHARED_H
+
+#include <ctype.h>
+#include <getopt.h>
+#include <signal.h>
+#include <stdarg.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <sys/stat.h>
+#include <sys/time.h>
+#include <sys/wait.h>
+#include <time.h>
+#include <unistd.h>
+
+#define MIN(x, y) (((x) < (y)) ? (x) : (y))
+
+void usage(int);
+void touchFile(char*);
+
+#endif
