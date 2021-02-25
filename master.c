c181043188b27fc2c36c6cef7da345b7c884142e
PhilDanW <42147737+PhilDanW@users.noreply.github.com>
2021-02-25 12:29:05 -0600

    Update master.c
    Rewrote master to start from scratch. Tried to do too much at once, starting with basics

--- master.c
+++ master.c
@@ -1,22 +1,16 @@
 #include "shared.h"
 
-/*Prototypes of functions*/
-int loadNumbers(char*);
-void setTimer(int);
-void spawnChild(int);
-void sigHandler(int);
-
 /*Set the option variables to their defaults to start with.
 * they will change later on*/
-int nChildren = PROCESS_MAX_DEFAULT;
-int s = CON_PROCESS_DEFAULT;
-int tSeconds = PROGRAM_TIME_MAX;
-
-/*Flag to disable interrupt handler*/
-bool interrupt = false;
+const int maxChildren = 20;
+const int maxSeconds = 100;
 
 /*main() function for controlling master*/
 int main(int argc, char** argv) {
+	
+		int option;
+		int numChildren = 20;
+		int numSeconds = 100;
       
     signal(SIGINT, sigHandler);
   
@@ -27,8 +21,9 @@ int main(int argc, char** argv) {
     openFile("sum.out");
 	  openFile("output.log");
 	
+		opterr = 0;
 	  /*Get arguments from the command line*/
-		while(int option = getopt(argc, argv, "hs:t:")) != -1) {
+		while(option = getopt(argc, argv, "hs:t:")) != -1) {
 				swtich (option) {
 					case 'h':
 									programUsage(argv[0]);
@@ -38,7 +33,7 @@ int main(int argc, char** argv) {
 											perror("Invalid timeout time '%s'", optarg);
 									}
                   else {
-                      nChildren = atoi(optarg);
+                      numChildren = atoi(optarg);
                   }
 									break;
 					case 't':
@@ -46,7 +41,7 @@ int main(int argc, char** argv) {
 											perror("Invalid timeout time '%s'", optarg);
 									}
                   else {
-                      tSeconds = atoi(optarg);
+                      numSeconds = atoi(optarg);
                   }
 									break;
           case '?':
@@ -58,131 +53,26 @@ int main(int argc, char** argv) {
                   return EXIT_FAILURE;
 					}
 		}
-		
-		/*Display the usage screen if any of the options are invalid*/
-    if (!check) 
-        programUsage(EXIT_FAILURE);
+										 
+		numChildren = MIN(numChildren, maxChildren);
+		numSeconds = MIN(numSeconds, maxSeconds);
     
     /*See if there was an input file give in the argument list*/
-    if (argv[optind] == null) {
+    int index = optind;
+		if(index < argc) {
+			char fileName[] = argv[index];
+			
+			printf("%d Max Children Processes", numChildren);
+			printf("%d Seconds on Timer", numSeconds);
+			printf("%s is the Input File", fileName);
+		}
+		else {
         perror("No input file given");
         programUsage(EXIT_FAILURE);
     }
-     
-    allocateSM();
-                     
-    int count = loadInts(argv[optind]);
-                     
-    /*now clamp the option arguments to their positions*/
-    n = MIN(count, TOTAL_PROCESSES);
-    s = MIN(s, n);
-    t = MIN(t, PROGRAM_TIME);
-                     
-    /*Simulate the program exiting instantly*/
-		if (n==0 || s==0 || t == 0) exit(EXIT_SUCCESS);
-								
-		/*set the number of programs to be in system to shared memory variable*/
-		spm->total = n;
-		
-    /*Use the timeout option to initialize the timer*/
-		setTimer(t);
-		
-    /*variables meant to help with child spawn logic*/
-		int i = 0;
-		int j = n;
-		int k = n;
-										 
-		/*Spawn the initial children*/
-		while (i < s)
-						spawnChild(i++);
-										
-		/*Loop until the number of processes is equal to 0*/
-		while(k > 0) {
-          /*wait for the child to exit*/
-					wait(NULL);
-      
-          /*write to the log at what time it finished*/
-					logOutput("output.log", "%s: Process %d finished\n", getTime(), n - k);
-			
-          /*try to spawn another child process*/
-					if (i < j) 
-							spawnChild(i++);
-			
-					k--;
-		}
-		
-		/*Remove the shared memory*/
-		removeSM();
-		
-    /*use check variable to make sure entire program has finished, exit accordingly*/
-		if (check) {
-				EXIT_SUCCESS;
-		}
-		else {
-				EXIT_FAILURE;
-		}
-
- int loadInts(char* path) {
-     FILE* file = fopen(path, "r");
-     if (file == NULL) crash("File open failure: could not load numbers");
-   
-     int i = 0;
-     char* line = NULL;
-     size_t len = 0;
-     ssize_t read;
-   
-     while ( i < n && (read = getline(&line, &len, file)) != -1) {
-            removeNewline(line);
-            spm->strings[i++], line);
-     }
-   
-     fclose(file);
-     if (line) free(line);
-   
-     return i;
- }
-                     
- void setTimer(const int t) {
-      struct sigaction action;
-      memset(&action, 0, sizeof(action));
-      action.sa_handler = signalHandler;
-      if (sigaction(SIGALRM, &action, NULL) != 0) crash("Failure to set signal for timer");
-   
-      struct itimerval timer;
-      timer.it_value.tv_sec = t;
-	    timer.it_value.tv_usec = t;
-	
-	    timer.it_interval.tv_sec = 0;
-	    timer.it_interval.tv_usec = 0;
-	
-	    if (setitimer(ITIMER_REAL, &timer, NULL) != 0) crash("Failed to set timer");
- }
-                     
- void spawnChild(const int i) {
-       pit_t pid = fork();
-   
-       if (pid == -1) perror("Failure to create child process");
-   
-       if (pid == 0) {
-             flag = true'
-               
-             if (i == 0) spm->pgid = getpid();
-             setpgid(0, spm->pgid);
-         
-             flag = false;
-         
-             logOutput("output.log", "%s: Process %d is starting up\n", getTime(), i);
-             
-             char id[256];
-             sprintf(id, "%d", i);
-         
-             execl("./bin_adder", "bin_adder", id, (char*) NULL);
-         
-             exit(EXIT_SUCCESS);
-       }
- }
-                     
- void sigHandler(int s) {
+}
+                                    
+void sigHandler(int s) {
        if (flag) sleep(1);
    
        char message[4096];
